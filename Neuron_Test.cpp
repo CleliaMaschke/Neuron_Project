c5e3697658438fa3e54910e59e2dc82457325f69
Maschke Clelia Elisabeth <cmaschke@IC-CO-IN-SC038.intranet.epfl.ch>
2017-10-21 22:17:40 +0200

    Test Membrane potential with I = 1.0 and I = 1.01

--- Neuron_Test.cpp
+++ Neuron_Test.cpp
@@ -1,21 +1,66 @@
 #include "Neuron.hpp"
+#include "Cortex.hpp"
 #include <iostream>
 #include <cmath>
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
-TEST(Neuron_Test, calculate_potential) {
-	/*Neuron n1;
-	Neuron n2;
-	n1.potential = potential_seuil + 1;
-	n2.potential = 0.0;
-	n1.Iext = 1.01;
-	n1.update(0.2, 100);
-	EXPECT_EQ(n2.potential, J);*/
+TEST(neuron_test, calculate_potential) {
+	
 	Neuron neuron(1.0);
-	//First update test
-	neuron.update(0.1, 100);
-	EXPECT_EQ(((20.0*(1.0-std::exp(-0.1/20.0))) - neuron.getMembranePotential()), 0);
+	neuron.resizeRingBuffer(1);
+	
+	//First update test with dt = 0.1 and step start = 1000 
+	neuron.update(1000);
+	EXPECT_EQ(((20.0*(1.0 - exp(-0.1/20.0))) - neuron.getMembranePotential()), 0);
+	
+	//Test for 3000 update
+	for(long i(0); i < 3000; ++i) {
+		neuron.update(1000);
+	}
+	
+	//Iext = 1.0 -> no spike but membrane potential near 20
+	EXPECT_LT(neuron.getMembranePotential(), 20.0);
 }
+
+TEST(neuron_test, time_spikes) 
+{
+	/*
+	//Iext = 1.01
+	Neuron neuron(1.01);
+	neuron.resizeRingBuffer(neuron.getDelay() / 0.2 + 1);
+	
+	//Test time between 100 to 400ms
+	for(long i(100); i <= 400; ++i) {
+		neuron.update(0.1, 100);
+	}
+	
+	EXPECT_EQ(neuron.time_spike.size(), 3);
+	EXPECT_FLOAT_EQ(neuron.time_spike[0], 192.4);
+	EXPECT_FLOAT_EQ(neuron.time_spike[1], 286.8);
+	EXPECT_FLOAT_EQ(neuron.time_spike[2], 381.2);
+	* */
+	
+	Cortex cortex;
+	//time start = 100ms and Iext = 1.01
+	cortex.setStepClock(1000);
+	cortex.setStepEnd(4000);
+	cortex.initialise_neuron(0, 1.01);
+	cortex.setNeuronInput(0, 1.01);
+	cortex.setNeuronInput(1, 0.0);
+	
+	for(long i(1000); i <= 4000; ++i) {
+		cortex.update_neuron(1000, 4000);
+	}
+	
+	EXPECT_EQ(cortex.neurons[0]->time_spike.size(), 3);
+	EXPECT_FLOAT_EQ(cortex.neurons[0]->time_spike[0], 192.4);
+	EXPECT_FLOAT_EQ(cortex.neurons[0]->time_spike[1], 286.8);
+	EXPECT_FLOAT_EQ(cortex.neurons[0]->time_spike[2], 381.2);
+	
+	EXPECT_EQ(cortex.neurons[1]->time_spike.size(), 0);
+	
+}
+	
 
 int main (int argc, char **argv)
 {
